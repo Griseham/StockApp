9dbf369b20e819135e84c4df4bbde45e9338c96e
Djimmitry Graham <81120298+Griseham@users.noreply.github.com>
2021-04-25 20:47:23 -0400

    Add files via upload

---
+++ DgrahamFinal/app/src/main/com.example.cop4655.Z23462230/Signin2.java
@@ -0,0 +1,23 @@
+package com.example.navbar;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Signin2 extends AppCompatActivity {
+
+    @Nullable
+
+
+
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstances){
+
+        return inflater.inflate(R.layout.fragment_signin, container, false);
+
+    }
+
+}
